1880979706e70a85f8c045f9ef231c7da118748e
Trong Nguyen <phantrongnguyen.1811@gmail.com>
2025-11-04 18:21:15 +0700

    add another  solution for No.209

--- 209_Minimum_Size_SubArray_Sum.cpp
+++ 209_Minimum_Size_SubArray_Sum.cpp
@@ -2,29 +2,46 @@
 #include <vector>
 using namespace std;
 
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int left = 0, right  = 0;
+//         int res = __INT_MAX__;
+//         int sum = nums[0];
+//         while (left <= right && right < nums.size()){
+//             if(sum >= target) {
+//                 if(right == left) return 1;
+//                 else res = (res > (right - left + 1)) ? (right - left + 1) : res;
+//                 sum = sum - nums[left];
+//                 left++;
+//             } 
+//             else {
+//                 if(right < nums.size() - 1) {
+//                     right++;
+//                     sum = sum + nums[right];
+//                 }
+//                 else break;
+//             }
+//         }
+//         return res = (res == __INT_MAX__) ? 0:res;
+//     }
+// };
+
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int left = 0, right  = 0;
-        int res = 0;
-        int sum = nums[0];
-        if(nums.size() == 1) return (nums[0] >= target) ? 1 : 0;
-        
-        while (left <= right && right < nums.size()){
-            if(sum >= target) {
-                if(right == left) return 1;
-                else {
-                    res = (res > (right - left + 1)) ? right - left + 1 : res;
-                }
-                sum = sum - nums[left];
+        int res = __INT_MAX__;
+        int sum = 0;
+        for(right = 0 ; right < nums.size()  ; right++){
+            sum += nums[right];
+            while(sum >= target){
+                res = min(res, right - left + 1);
+                sum-=nums[left];
                 left++;
-            } 
-            else {
-                right++;
-                sum = sum + nums[right];
             }
         }
-        return res;
+        return res = (res == __INT_MAX__) ? 0:res;
     }
 };
 
